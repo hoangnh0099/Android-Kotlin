788495ab614022f9e4c8e76814ec31dc5e4d3b88
Nguyen Huy Hoang <hoangnh0099@gmail.com>
2020-03-03 01:18:35 +0700

    Update

---
+++ ExampleButton/app/src/main/java/com/example/examplebutton/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.examplebutton
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        button.setOnClickListener {
+            Toast.makeText(this, "Hello World", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
