449887466a3cb0010ddd1e1c22b9705a67c01991
Nguyen Huy Hoang <hoangnh0099@gmail.com>
2020-03-03 17:33:33 +0700

    Update

---
+++ ExampleImageView/app/src/main/java/com/example/exampleimageview/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.exampleimageview
+
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        linearLayout.setBackgroundColor(Color.GREEN)
+        imageView.setImageResource(R.drawable.ic_launcher_foreground)
+    }
+}
